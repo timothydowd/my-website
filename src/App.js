907398cdb1fd52c674d6efba9c9e3b5dc0e4453c
timothydowd <timdowd1984@gmail.com>
2019-05-20 11:42:39 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+
+
 import './App.css';
 
 function App() {
+
+  library.add(faStroopwafel)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <span><button>work</button>&nbsp;<button>about</button>&nbsp;<button>contact</button></span>
+
+
     </div>
   );
 }
