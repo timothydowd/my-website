c66c9e1f61b844017d2f1bae1fa271da8763aa4f
timothydowd <timdowd1984@gmail.com>
2019-05-20 18:01:09 +0100

    tech logos added and made responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 
 
 import '../Header.css';
+import '../../node_modules/devicon/devicon.css'
+
 
 
 
 function Header() {
 
-    
-    
   return (
       <header>
           <h1>Tim Dowd</h1>
           <h3>Software Developer / Northcoders Graduate / Peanut Butter Fan</h3>
-        
-
-
+          <p></p>
+          <i className="devicon-javascript-plain"></i>
+          <i className="devicon-react-original"></i>
+          <i class="devicon-express-original"></i>
+          <i class="devicon-html5-plain"></i>
+          <i class="devicon-css3-plain"></i>
+          <i class="devicon-bootstrap-plain"></i>
+          <i class="devicon-mocha-plain"></i>
+          <i class="devicon-git-plain"></i>
       </header>
 
   );
