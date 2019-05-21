16688791f23a0202572133f822b5416d255ea8b9
timothydowd <timdowd1984@gmail.com>
2019-05-21 10:49:18 +0100

    contact logos added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+import '../../node_modules/devicon/devicon.css'
 import '../Contact.css';
 
 
@@ -12,6 +12,11 @@ function Contact() {
   return (
       <div className='contact-container'>
         <p>contact</p>
+        {/* <i class="devicon-github-plain"></i> */}
+        <img src={require(".././images/linkedin.svg")} alt="linkedin" />
+        <img src={require(".././images/github.svg")} alt="github" />
+        <img src={require(".././images/email.svg")} alt="email" />
+
         
       </div>
 
