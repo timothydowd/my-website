b0b9104dfa46cb05b57d6b59734de10ec34428ce
timothydowd <timdowd1984@gmail.com>
2019-05-31 15:38:44 +0100

    about section updated and padding added to header to fix space at bottom

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,21 +17,22 @@ function About() {
 
           <p className='about-text'>
             After becoming obsessed with automating google sheets I decided to dip my toe a 
-            little deeper intot the world of code..  I started studying Ruby and a few months 
+            little deeper into the world of code..  I started studying Ruby and a few months 
             later had enrolled myself to Northcoders bootcamp based in Manchester. From here I 
             learnt fundamental Javascript which led to me creating fullstack web applications 
             using Node.js, Express.js, Postgresql and React.
           </p>
 
           <p className='about-text'>
-            Being a very visual person, my passion tends to lean towards front end.
+            Being a very visual person, my passion tends to lean towards front end; I seem to
+            get a kick out of making things look nice whilst also being functional, which perhaps
+            lends from having a degree in Product Design.
           </p>
 
           <p className='about-text'>
             In my spare time you can find me playing around in Photoshop and Illustrator, making
-            awful music in Ableton, running around the streets of Manchester trying to keep fit
-            or doing the occasional lightning talk.  But most likely you will find me playing 
-            Apex Legends
+            ill attempts of music in Ableton, running around the streets of Manchester trying to keep fit
+            or doing the occasional lightning talk.
           </p>
 
         </div>
